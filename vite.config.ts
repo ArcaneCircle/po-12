ac7466f927efbbc18e67e9f1554ffa5c08976bd2
adbenitez <asieldbenitez@gmail.com>
2026-04-21 18:30:18 +0200

    port to webxdc

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
+import { buildXDC, eruda, mockWebxdc } from "@webxdc/vite-plugins";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import { resolve } from "node:path";
 
+function xdcFilter(fileName, filePath, isDirectory) {
+  return !(
+    fileName.endsWith("~")
+    || fileName === "favicon.ico"
+    || fileName === "favicons"
+  );
+}
+
 export default defineConfig({
   plugins: [
+    buildXDC({filter: xdcFilter}),
+    eruda(),
+    mockWebxdc(),
     react(),
     VitePWA({
       registerType: "autoUpdate",
